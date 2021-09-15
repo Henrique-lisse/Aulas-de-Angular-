5922b26e883fd1b204fc647e8439d5d9d8110d9d
Henrique Lisse Da Silva <henrique_silva1210@outlook.com>
2021-09-15 20:30:22 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './template/header/header.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ListarProdutosComponent } from './components/produtos/listar-produtos/listar-produtos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ListarProdutosComponent
   ],
   imports: [
     BrowserModule,
